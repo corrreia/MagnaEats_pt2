1ea630f8d0d36527ccbc112cefe2f9be08a78e54
RafaelNisa <rafaelnisa.2001@hotmail.com>
2022-05-18 19:39:53 +0100

    Alterações ao log e a main

--- include/mesignal.h
+++ include/mesignal.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #ifndef MESIGNAL_H_GUARD
 #define MESIGNAL_H_GUARD
 
@@ -5,6 +6,17 @@
 
 void createAlarm(int alarmTime);
 
-//void doAlarm(struct *operation results);
+void doAlarm(struct *operation results);
 
+=======
+#ifndef MESIGNAL_H_GUARD
+#define MESIGNAL_H_GUARD
+
+#include "main.h"
+
+void createAlarm(int alarmTime);
+
+void doAlarm(struct *operation results);
+
+>>>>>>> 944643a45a3dad45a22fe0789c5d1f23a4b1f33e
 #endif
