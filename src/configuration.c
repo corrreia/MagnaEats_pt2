1c80bffc670d76186e6eefb2ce41bf40e6109252
RafaelNisa <rafaelnisa.2001@hotmail.com>
2022-05-18 19:40:24 +0100

    Alterações ao log e a main

--- src/configuration.c
+++ src/configuration.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -32,4 +33,36 @@ void lerFicheiro(struct main_data* data, char fileName[]){
 
     //close file
     fclose(in_file);
+=======
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/configuration.h"
+#include "../include/main.h"
+
+
+void lerFicheiro(struct main_data* data, char fileName[]){
+
+    //Open file
+    FILE *in_file  = fopen(fileName, "r"); // read only 
+
+    fscanf(in_file,"%d",  &data -> max_ops);
+    fscanf(in_file,"%d",  &data -> buffers_size);
+    fscanf(in_file,"%d",  &data -> n_restaurants);
+    fscanf(in_file,"%d",  &data -> n_drivers);
+    fscanf(in_file,"%d",  &data -> n_clients);
+
+    char ts [30];   //talvez alterar?
+    fscanf(in_file,"%s", ts);
+    strcpy(data -> log_filename,ts);
+
+    char t1 [30];  //talvez alterar?
+    fscanf(in_file,"%s",  t1);
+    strcpy(data -> statistics_filename,t1);
+
+    fscanf(in_file,"%d",  &data -> alarm_time);
+
+    //close file
+    fclose(in_file);
+>>>>>>> 944643a45a3dad45a22fe0789c5d1f23a4b1f33e
 }
--- src/log.c
+++ src/log.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "../include/log.h"
 #include <stdio.h>
 #include <string.h>
@@ -40,4 +41,46 @@ void getTime(){
 
 void closeFile(){
     closeFile(log_file);
+=======
+#include "../include/log.h"
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+FILE * createLogFile(char * filename){
+    return fopen (filename, "w");
+}
+
+void writeRead(int op, FILE * log_file){
+    getTime(log_file);
+    fprintf(log_file,"%s %d\n","read",op);
+}
+
+void writeStop(FILE * log_file){
+    getTime(log_file);
+    fprintf(log_file,"%s\n","stop");
+}
+
+void writeOp(int client_id,int restaurant_id,char * dish, FILE * log_file){
+    getTime(log_file);
+    fprintf(log_file,"%s %d %d %s\n","op", client_id, restaurant_id,dish);
+
+}
+void writeHelp(FILE * log_file){
+    getTime(log_file);
+    fprintf(log_file,"%s\n","help");
+}
+
+void getTime(FILE * log_file){
+    struct timespec timer;
+    clock_gettime( CLOCK_REALTIME, &timer);
+    char t [40];
+    strftime(t,40,"%F %X",localtime(&timer.tv_sec));
+
+    fprintf(log_file,"%s.%ld ",t,timer.tv_nsec/1000000);
+}
+
+void closeFile(FILE * log_file){
+    closeFile(log_file);
+>>>>>>> 944643a45a3dad45a22fe0789c5d1f23a4b1f33e
 }
